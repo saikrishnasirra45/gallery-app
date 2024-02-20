ad8a70d49210517469deb7eb0d413dbeae029a3e
saikrishnasirra45 <saikrishnasirra@gmail.com>
2024-02-20 10:14:25 +0530

    feature signup working

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,27 @@
+import './index.css'
+import Gallery from '../Gallery'
+const ThumbnailItem = props => {
+  const {imageDetails, isActive, setActiveThumbnailId} = props
+  const {thumbnailUrl, thumbnailAltText, id} = imageDetails
+  const thumbnailClassName = isActive ? `thumbnail active` : `thumbnail`
+
+  const onClickThumbnail = () => {
+    setActiveThumbnailId(id)
+  }
+  return (
+    <li className="thumbnail-list-item">
+      <button
+        type="button"
+        className="thumbnail-button"
+        onClick={onClickThumbnail}
+      >
+        <img
+          src={thumbnailUrl}
+          alt={thumbnailAltText}
+          className={thumbnailClassName}
+        />
+      </button>
+    </li>
+  )
+}
+export default ThumbnailItem
